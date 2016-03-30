c25965aa848d268fb5abb61d9034e557ed3262f2
Rahul Huilgol <rahulhuilgol@gmail.com>
2016-03-30 18:44:01 -0500

    finish pos

--- pos/convert_data.sh
+++
@@ -1,3 +0,0 @@
-javac DataConverter.java WordFeatures.java
-java -cp ../ pos.DataConverter /projects/nlp/penn-treebank3/tagged/pos/atis/ ../../data/atisMax suffixes.txt
-java -cp ../ pos.DataConverter /projects/nlp/penn-treebank3/tagged/pos/wsj/ ../../data/wsjMax suffixes.txt
--- pos/copy.sh
+++
@@ -1,6 +0,0 @@
-cp HMMSimpleTagger.java ../../mallet-2.0.8RC3/src/cc/mallet/fst/
-cp SimpleTagger.java ../../mallet-2.0.8RC3/src/cc/mallet/fst/
-cp TokenAccuracyEvaluator.java ../../mallet-2.0.8RC3/src/cc/mallet/fst/
-cd ../../mallet-2.0.8RC3
-ant
-cd ../nlp/pos
